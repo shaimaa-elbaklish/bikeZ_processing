e404cbc2bfa63c98772969da7f61e10ce2bca7c0
Ying-Chuan Ni <102191393+YCNi@users.noreply.github.com>
2025-11-21 15:38:56 +0100

    delete unused file

--- src/main_lane_coord_transform.py
+++
@@ -1,9 +0,0 @@
-
-from tools_coordinateTransform import (extract_roadway_centerline, project_point_onto_spline,
-                                       convert_utm_to_roadway_coordinates, convert_roadway_to_utm_coordinates)
-
-tck, unew, cum_dist = extract_roadway_centerline("BIKEZ")
-
-for loop ...
-t_star, closest_point = project_point_onto_spline(point, tck)
-t_star, tangent, normal, s, d = convert_utm_to_roadway_coordinates(point, tck, unew, cum_dist)
